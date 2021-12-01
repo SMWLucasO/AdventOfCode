8e0d0c95519c47364200f291b525d2791310e8f9
Lucas <lucasouwens@gmail.com>
2021-12-01 12:43:00 +0100

    Day 1 solutions for AoC 2021

---
+++ AdventOfCode/Day1/DepthIncrease.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Day1
+{
+    public static class DepthIncrease
+    {
+
+        public static int Solution(List<int> input)
+        {
+            if (input.Count <= 1)
+                return 0;
+
+            int increments = 0;
+
+            for (int i = 0; i < input.Count - 1; i++)
+            {
+                // verschil check, is het verschil positief? Dan hebben we een increase.
+                if (input[i + 1] - input[i] > 0)
+                    increments += 1;
+            }
+
+            return increments;
+        }
+
+        public static int SolutionSlidingWindow(List<int> input)
+        {
+
+            // Remove from start
+            // Add to end
+            
+            // Two vars: current and next.
+
+            int solution = 0;
+            int currentSum = 0;
+            
+
+            // sliding window gedoe
+            int left = 0;
+            int right = 0;
+
+            while(right < input.Count)
+            {
+                if (right < 3)
+                {
+                    currentSum += input[right++];
+                    continue;
+                }
+
+                // Haal van de tail af, voeg aan de head toe. Zo kan je 't zien.
+                int nextSum = (currentSum - input[left++]) + input[right++];
+
+                // volgende beter dan huidige? Dan increment.
+                if (nextSum > currentSum)
+                    solution += 1;
+
+                currentSum = nextSum;
+
+            }
+            
+            return solution;
+        }
+        
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventOfCode.Day1;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // day 1
+            var input = File.ReadAllLines("Resources/Day1/input.txt")
+                .Select(str => Convert.ToInt32(str)).ToList();
+            
+            Console.WriteLine(DepthIncrease.Solution(input));
+            Console.WriteLine(DepthIncrease.SolutionSlidingWindow(input));
+
+        }
+    }
+}
